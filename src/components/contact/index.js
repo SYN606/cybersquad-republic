ef7efba9a5e1c1e95abf3f75dc94a88657362d09
bhavinithakur29 <bhavinithakurshan@gmail.com>
2025-08-17 20:51:08 +0100

    MAJOR: contact page setup; added some contact data; added faq section; added contact form frontend

---
+++ src/components/contact/index.js
@@ -0,0 +1,5 @@
+import ContactHero from "./ContactHero";
+import ContactForm from "./ContactForm";
+import FAQ from "./FAQ";
+
+export { ContactHero, ContactForm, FAQ }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,11 @@
+import { ContactHero, ContactForm, FAQ } from "../components/contact";
+
 export default function Contact() {
     return (
         <>
-            <h2 className="text-2xl sm:text-3xl font-bold text-white mb-6 sm:mb-8">Contact Us</h2>
+            <ContactHero />
+            <ContactForm />
+            <FAQ />
         </>
     );
 } 
